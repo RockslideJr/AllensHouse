fb526f715181105e5b8cf7b0bbb04921c9d59813
John Borden <jborden36@gmail.com>
2017-07-24 18:01:29 -0700

    I'm Learning!

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-var express = require('express')
+const express = require('express')
+const pug = require('pug')
 
-var app = express()
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'pug')
+
+app.use('/assets', express.static(`${ __dirname }/public`))
+
+const index = pug.renderFile('views/index.pug', {title: 'Allen\'s House', content: 'Hello World'})
+
+app.get('/', function (req, res){
+    res.render('index', {title: 'Allen\'s House', content: 'Hello WOrld'})
+})
+
+app.listen(port)
